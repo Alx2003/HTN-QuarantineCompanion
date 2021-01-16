5d41bc6707a3e2e9a9c178de6d9ce33b02287157
Alx2003 <alexanderqlay@gmail.com>
2021-01-16 18:11:43 -0500

    dark mode text

--- views/darkMode.js
+++ views/darkMode.js
@@ -2,5 +2,14 @@ document.body.onkeyup = function(e) {
     if(e.code == "Space"){
       document.body.classList.toggle
       ("dark");
+
+      var messages = new Array();
+      messages[0] = "Drink lots of water!"
+      messages[1] = "Eat healthy food!"
+      messages[2] = "Get lots of sleep!"
+      messages[3] = "You can do this!"
+      messages[4] = "Don't forget to keep active!"
+      
+      var message = messages[Math.floor(Math.random()*messages.length)];
     }
   }
